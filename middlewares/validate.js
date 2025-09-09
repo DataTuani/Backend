878db89c4a08b8785bdd4f9bc81a1a83f47ec98d
Yasser <yassercomunica@gmail.com>
2025-09-09 15:50:41 -0600

    Response Update Auth

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -3,13 +3,14 @@ const { validationResult } = require("express-validator");
 
 const validate = (validations) => {
   return async (req, res, next) => {
-    await Promise.all(validations.map(v => v.run(req)));
+    await Promise.all(validations.map((v) => v.run(req)));
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
+    console.log("PARAMS:", req.params);
+    console.log("BODY:", req.body);
     next();
   };
 };
